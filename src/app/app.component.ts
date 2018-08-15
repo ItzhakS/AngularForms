7df3bce857038205ba3836dfd7a518db1f0d13bf
ItzhakS <ieshachnovitz@gmail.com>
2018-08-15 20:04:30 +0300

    Create Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollService } from './enroll.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  topics = ['BAseball','Angular','Buttons','Windows','Linux'];
+
+  topicHasError = true;
+
+  userModel = new User('Rob','a@a.com', 1234543, "default", 'morning', true);
+
+  constructor(private _enrollmentService:EnrollService){}
+
+  validateSelect(value){
+    if(value == 'default') this.topicHasError = true;
+    else this.topicHasError = false;
+  }
+
+  onSubmit(){
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success', data),
+      error => console.log('Error', error)
+      )
+  }
 }
